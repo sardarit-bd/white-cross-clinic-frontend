f5223e62c81db484b42cbfef2d9998002a2c05ea
Md <sohelf131@gmail.com>
2025-11-27 14:56:06 +0600

    changed auth function

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,12 +1,10 @@
-import { useSearchParams } from "next/navigation";
+
 
 export const useAuth = () => {
-  const role = useSearchParams().get('role')
-  console.log(role)
   const user = {
     id: 1,
     name: "Md Sohel",
-    role: role || "doctor", // Change to 'admin' or 'student' to test different roles
+    role: "doctor", // Change to 'admin' or 'student' to test different roles
   };   
 
   return user; 
